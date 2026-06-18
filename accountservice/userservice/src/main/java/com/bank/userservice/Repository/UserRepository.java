77612b79ee9efc3bef99d2ac1f3d12255edeb9d2
charan <sricharan064@gmail.com>
2026-06-18 17:10:31 +0530

    removed user service

--- accountservice/userservice/src/main/java/com/bank/userservice/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.userservice.Repository;
-
-import com.bank.userservice.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-    // Implementation are not needed becoz automatically provides ..
-}
